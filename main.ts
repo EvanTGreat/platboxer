0334163b74562eedc713dcd18f6500d9fdb29929
EvanTGreat <142271031+EvanTGreat@users.noreply.github.com>
2024-01-16 17:40:39 -0500

    skate pwr up + punch

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.field0, function (sprite, location) {
+    tiles.setTileAt(location, sprites.builtin.field1)
+    power_up = randint(0, 5)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
     if (controller.B.isPressed()) {
         level = next_lvl
@@ -16,13 +20,19 @@ function hurt_time () {
     })
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    hurt_time()
+    if (controller.B.isPressed()) {
+        sprites.destroy(otherSprite, effects.ashes, 200)
+    } else {
+        hurt_time()
+    }
 })
 let mySprite2: Sprite = null
+let power_up = 0
 let next_lvl = 0
 let level = 0
 let spawn_control = 0
 let hurt = 0
+let punch = 0
 hurt = 0
 spawn_control = 0
 level = 0
@@ -163,6 +173,7 @@ mySprite.setScale(0.6, ScaleAnchor.Middle)
 music.play(music.createSong(hex`0078000408020504001c00100500640000041e000004000000000000000000000000000a04000419000c00100003191b1d340038000c0506080a0c0d0f111214161805001c000f0a006400f4010a00000400000000000000000000000000000000020c0004000800012c2c003000012907001c00020a006400f401640000040000000000000000000000000000000003120014001800011918001c00011d1c002000012008001c000e050046006603320000040a002d0000006400140001320002010002060024002800012c09010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c80008000c000d0003000102`), music.PlaybackMode.LoopingInBackground)
 info.setLife(3)
 forever(function () {
+    power_up = 5
     if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
         mySprite.setImage(assets.image`myImage`)
     }
@@ -215,9 +226,60 @@ forever(function () {
             .....ffffff.....ffffff........
             `)
     }
+    if (0 < level) {
+        mySprite.ay = 1000
+        if (power_up == 5) {
+            mySprite.setImage(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `)
+            controller.moveSprite(mySprite, 150, 0)
+            if (controller.A.isPressed()) {
+                if (mySprite.vy == 0) {
+                    mySprite.vy = -652
+                }
+            }
+        } else {
+            controller.moveSprite(mySprite, 69, 0)
+            if (controller.A.isPressed()) {
+                if (mySprite.vy == 0) {
+                    mySprite.vy = -300
+                }
+            }
+        }
+        if (controller.B.isPressed()) {
+            if (punch == 0) {
+                timer.background(function () {
+                    mySprite.setImage(assets.image`myImage0`)
+                    pause(5000)
+                    punch = 1
+                    mySprite.setImage(assets.image`myImage`)
+                    punch = 0
+                })
+            } else {
+                if (!(power_up == 5)) {
+                    mySprite.setImage(assets.image`myImage`)
+                }
+            }
+        }
+    }
     if (level == 1) {
-        tiles.setCurrentTilemap(tilemap`level0`)
         if (spawn_control == 0) {
+            tiles.setCurrentTilemap(tilemap`level0`)
             tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 43))
             mySprite2 = sprites.create(img`
                 ..............................
@@ -266,21 +328,11 @@ forever(function () {
                 .....777766.....777666........
                 .....ffffff.....ffffff........
                 `, SpriteKind.Enemy)
+            mySprite2.ay = 1000
             mySprite2.setScale(0.6, ScaleAnchor.Middle)
             tiles.placeOnTile(mySprite2, tiles.getTileLocation(19, 42))
             mySprite2.follow(mySprite, 30)
             spawn_control = 1
         }
-        if (controller.B.isPressed()) {
-        	
-        }
-        mySprite2.ay = 1000
-        mySprite.ay = 1000
-        if (controller.A.isPressed()) {
-            if (mySprite.vy == 0) {
-                mySprite.vy = -300
-            }
-        }
-        controller.moveSprite(mySprite, 69, 0)
     }
 })
